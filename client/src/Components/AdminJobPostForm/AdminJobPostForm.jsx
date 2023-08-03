725f25f638600f6cef0c8548323deae565741698
Shone Antony <shonantony@gmail.com>
2023-08-04 00:14:09 +0100

    updated with job vacancy posting from admin

--- client/src/Components/AdminJobPostForm/AdminJobPostForm.jsx
+++ client/src/Components/AdminJobPostForm/AdminJobPostForm.jsx
@@ -13,8 +13,9 @@ function AdminJobPostForm() {
   const [expiry, setExpiry] = useState('')
   const [isVerified, setIsVerified] = useState(false)
 
-  const handleJobPost = async() =>{
-    try {
+  const handleJobPost = async (e) => {
+    e.preventDefault()
+
     const jobPostData = {
       jobTitle: jobTitle,
       location: location,
@@ -25,22 +26,24 @@ function AdminJobPostForm() {
       expiry: expiry
     }
 
-    const res = await axios.post('/admin/post_job_vacancy', jobPostData)
-    console.log(res.data);
+    try {
 
-    if(res.status === 200){
-      setIsVerified(true)
-    } else {
-      setIsVerified(false)
-    }   
-      
+      const res = await axios.post('/admin/post_job_vacancy', jobPostData)
+      console.log(res.data);
+
+      if (res.status === 200) {
+        setIsVerified(true)
+      } else {
+        setIsVerified(false)
+      }
+
     } catch (error) {
-      console.error(error)      
+      console.error('Error occurred while posting job:', error)
     }
   }
 
-  if(isVerified){
-    <Navigate to='/admin_dashboard' />
+  if (isVerified) {
+    return <Navigate to='/admin_dashboard' />
   }
 
   return (
@@ -51,51 +54,51 @@ function AdminJobPostForm() {
         </p>
         <form onSubmit={handleJobPost} className='flex flex-col'>
 
-        <input type="text" 
-        className='w-96 h-10 bg-white font-sans text-black mt-10 rounded-md shadow-lg' 
-        name='jobTitle' value={jobTitle}
-        onChange={(e) => setJobTitle(e.target.value)}
-        placeholder='  Enter Job Title' />
+          <input type="text"
+            className='w-96 h-10 bg-white font-sans text-black mt-10 rounded-md shadow-lg'
+            name='jobTitle' value={jobTitle}
+            onChange={(e) => setJobTitle(e.target.value)}
+            placeholder='  Enter Job Title' />
 
-        <input type="text" 
-        className='w-96 h-10 bg-white font-sans text-black mt-2 rounded-md shadow-lg' 
-        name='location' value={location}
-        onChange={(e) => setLocation(e.target.value)}
-        placeholder='  Enter Location' />
+          <input type="text"
+            className='w-96 h-10 bg-white font-sans text-black mt-2 rounded-md shadow-lg'
+            name='location' value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            placeholder='  Enter Location' />
 
-        <input type="text" 
-        className='w-96 h-10 bg-white font-sans text-black mt-2 rounded-md shadow-lg'
-        name='salaryMin' value={salaryMin} 
-        onChange={(e) => setSalaryMin(e.target.value)}
-        placeholder='  Enter Salary Minimum' />
+          <input type="text"
+            className='w-96 h-10 bg-white font-sans text-black mt-2 rounded-md shadow-lg'
+            name='salaryMin' value={salaryMin}
+            onChange={(e) => setSalaryMin(e.target.value)}
+            placeholder='  Enter Salary Minimum' />
 
-        <input type="text" 
-        className='w-96 h-10 bg-white font-sans text-black mt-2 rounded-md shadow-lg' 
-        name='salaryMax' value={salaryMax}
-        onChange={(e) => setSalaryMax(e.target.value)}
-        placeholder='  Enter Salary Maximum' />
+          <input type="text"
+            className='w-96 h-10 bg-white font-sans text-black mt-2 rounded-md shadow-lg'
+            name='salaryMax' value={salaryMax}
+            onChange={(e) => setSalaryMax(e.target.value)}
+            placeholder='  Enter Salary Maximum' />
 
-        <input type="text" 
-        className='w-96 h-10 bg-white font-sans text-black mt-2 rounded-md shadow-lg'
-        name='jobType' value={jobType}
-        onChange={(e) => setJobType(e.target.value)}
-        placeholder='  Enter Job Type' />
+          <input type="text"
+            className='w-96 h-10 bg-white font-sans text-black mt-2 rounded-md shadow-lg'
+            name='jobType' value={jobType}
+            onChange={(e) => setJobType(e.target.value)}
+            placeholder='  Enter Job Type' />
 
-        <input type="text" 
-        className='w-96 h-10 bg-white font-sans text-black mt-2 rounded-md shadow-lg' 
-        name='jobDescription' value={jobDescription}
-        onChange={(e) => setJobDescription(e.target.value)}
-        placeholder='  Enter Job Description' />
+          <input type="text"
+            className='w-96 h-10 bg-white font-sans text-black mt-2 rounded-md shadow-lg'
+            name='jobDescription' value={jobDescription}
+            onChange={(e) => setJobDescription(e.target.value)}
+            placeholder='  Enter Job Description' />
 
-        <input type="text" 
-        className='w-96 h-10 bg-white font-sans text-black mt-2 mb-10 rounded-md shadow-lg' 
-        name='expiry' value={expiry} 
-        onChange={(e) => setExpiry(e.target.value)}
-        placeholder='  Enter Expiry Date' />
+          <input type="text"
+            className='w-96 h-10 bg-white font-sans text-black mt-2 mb-10 rounded-md shadow-lg'
+            name='expiry' value={expiry}
+            onChange={(e) => setExpiry(e.target.value)}
+            placeholder='  Enter Expiry Date' />
 
-        <button className='w-96 h-10 bg-sky-700 text-lg font-semibold text-white mt-3 mb-10 rounded-md'>    
-          Post Job
-        </button> 
+          <button className='w-96 h-10 bg-sky-700 text-lg font-semibold text-white mt-3 mb-10 rounded-md'>
+            Post Job
+          </button>
 
         </form>
       </div>
--- client/src/Components/AdminTabs/AdminTabs.jsx
+++ client/src/Components/AdminTabs/AdminTabs.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function AdminTabs() {
 
+  
+
   return (
     <>
+
       <div className='container mx-auto px-20 pt-10 grid grid-cols-4 gap-y-5'>
-        <button className='w-60 h-60 bg-sky-700 rounded-2xl flex justify-center items-center'>
-          <p className='text-white font-bold text-lg text-center'>
-            POST JOB VACANCY
-          </p>
-        </button>
+        <Link to={`/admin_job_post`}>
+          <button className='w-60 h-60 bg-sky-700 rounded-2xl flex justify-center items-center'>
+            <p className='text-white font-bold text-lg text-center'>
+              POST JOB VACANCY
+            </p>
+          </button>
+        </Link>
         <button className='w-60 h-60 bg-sky-700 rounded-2xl flex justify-center items-center'>
           <p className='text-white font-bold text-lg text-center'>
             JOB APPLICATION
@@ -37,7 +43,7 @@ function AdminTabs() {
         </button>
         <button className='w-60 h-60 bg-sky-700 rounded-2xl flex justify-center items-center'>
           <p className='text-white font-bold text-lg text-center'>
-           TASK
+            TASK
           </p>
         </button>
       </div>
