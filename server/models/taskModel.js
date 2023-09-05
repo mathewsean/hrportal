466b09353507c65cc9f46716b040925bd6a8e98e
Shone Antony <shonantony@gmail.com>
2023-09-05 13:54:05 +0100

    added task schema

---
+++ server/models/taskModel.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const taskSchema = mongoose.Schema({
+
+  taskName:{
+    type: String,
+    required:true
+  },
+  employeeId:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Candidate',
+    required:true
+  },
+  createdAt: {
+    type: Date,
+    required: true
+  },
+  startDate: {
+    type: Date,
+    required: true
+  },
+  endDate:{
+    type: Date,
+    required: true
+  },
+  status:{
+    type: String,
+    enum: ["Pending", "Work in Progress", "Completed"],
+    default: "Pending"
+  }
+
+})
+
+const Task = mongoose.model('Task', taskSchema)
+export default Task
